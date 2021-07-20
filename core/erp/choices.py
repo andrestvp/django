ea25b0bcbc462410b1bf767f33580fa905a98ed7
andrestvp <andrest.9324@gmail.com>
2021-07-20 17:23:23 -0500

    martt

---
+++ core/erp/choices.py
@@ -0,0 +1,12 @@
+gender_choices = (
+    ('male','Masculino'),
+    ('female','Femenino'),
+)
+
+
+state_purchase = (
+    ('solicitud','Solicitud'),
+    ('aprobada', 'Aprobada'),
+    ('orden','Orden'),
+    ('rechazada','Rechazada'),
+)
