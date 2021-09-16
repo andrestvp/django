03fe1c8f7a18ea6d2844e7949548db9568f3afbf
andrestvp <andrest.9324@gmail.com>
2021-09-16 17:16:52 -0500

    p

---
+++ core/pos/admin.py
@@ -0,0 +1,66 @@
+#from core.pos.models import Purchase
+from core.pos.models import Sale
+from django.contrib import admin
+from core.pos.models import PurchaseRequest
+
+
+# Register your models here.
+#class SaleAdmin(admin.ModelAdmin):
+#    list_display=(
+#        'client',
+#        'payment_condition',  
+#        'sucursal',  
+#        'plazo',
+#        'estado',
+
+#    )
+#    search_fields = ('client',)
+
+#admin.site.register(Sale, SaleAdmin)
+
+class PurchaseRequestDetailAdmin(admin.ModelAdmin):
+    list_display=(
+        #'purchaserequest',
+        'product',  
+        'cant',  
+
+
+    )
+    search_fields = ('product',)
+
+
+
+
+class PurchaseRequestAdmin(admin.ModelAdmin):
+    list_display=(
+        #'purchaserequest',
+        'state',  
+
+
+    )
+    search_fields = ('state',)
+admin.site.register(PurchaseRequest, PurchaseRequestAdmin)
+#admin.site.register(Sale, SaleAdmin)
+
+#class PurchaseAdmin(admin.ModelAdmin):
+#    list_display=(
+#            'provider',
+#            'payment_condition',
+#            'sucursal',
+#            'state'
+#)
+#admin.site.register(Purchase, PurchaseAdmin)
+
+#PurchaseDetail
+#class PurchaseRequestAdmin(admin.ModelAdmin):
+#    list_display=(
+#        'reference',
+#        'date_joined',  
+#        'state',  
+
+
+#    )
+#    search_fields = ('reference',)
+
+#admin.site.register(PurchaseRequestAdmin, PurchaseRequest)
+
